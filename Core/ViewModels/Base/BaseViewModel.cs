131d9f80b8c8145a141b22db5ac753cf6bcb545c
Paul Patarinski <paulpatarinski@gmail.com>
2015-02-01 09:20:59 -0600

    Using the Xamarin Forms BaseViewModel

--- Core/ViewModels/Base/BaseViewModel.cs
+++ Core/ViewModels/Base/BaseViewModel.cs
@@ -1,82 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using XLabs.Forms.Mvvm;
+﻿using XLabs.Forms.Mvvm;
 
 namespace Core.ViewModels.Base
 {
-  public class BaseViewModel : ViewModel, INotifyPropertyChanged
+  public class BaseViewModel : NavigationAwareViewModel
   {
-    //Standard IsBusy property
-    private bool _isbusy;
-
-    public bool IsBusy
-    {
-      get { return _isbusy; }
-      set { SetField(ref _isbusy, value); }
-    }
-
-    // boiler-plate
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    protected virtual void OnPropertyChanged(string propertyName)
-    {
-      var handler = PropertyChanged;
-      if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
-    }
-
-    protected bool SetField<T>(ref T field, T value, Action action = null, IEnumerable<string> additionalprops = null,
-      [CallerMemberName] string propertyName = null)
-    {
-      if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-      field = value;
-      OnPropertyChanged(propertyName);
-      //Check for related fields
-      if (additionalprops != null)
-      {
-        foreach (var s in additionalprops)
-          OnPropertyChanged(s);
-      }
-      ;
-      //Fire any post set action that was supplied
-      if (action != null) action();
-      return true;
-    }
-  }
-
-  public class ReadOnlyWrapperViewModel<T> : BaseViewModel
-  {
-    private T _item;
-
-    public ReadOnlyWrapperViewModel(T item)
-    {
-      Item = item;
-    }
-
-    public T Item
-    {
-      get { return _item; }
-      private set { SetField(ref _item, value); }
-    }
-  }
-
-  public class ReadOnlySelectable<T> : BaseViewModel
-  {
-    //Selectable Support
-    private bool _selected;
-
-    public ReadOnlySelectable(T item)
-    {
-      Item = item;
-    }
-
-    public T Item { get; private set; }
-
-    public bool Selected
-    {
-      get { return _selected; }
-      set { SetField(ref _selected, value); }
-    }
+   
   }
 }
