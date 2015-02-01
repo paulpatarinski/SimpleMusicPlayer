ee049578a957721fe864d72233dca29130429143
Paul Patarinski <paulpatarinski@gmail.com>
2015-02-01 14:22:08 -0600

    Adding a Fonts class

---
+++ Core/Helpers/Codes/Fonts.cs
@@ -0,0 +1,76 @@
+﻿using Xamarin.Forms;
+
+namespace Core.Helpers.Codes
+{
+  public static class Fonts
+  {
+    public static Font LargeBold
+    {
+      get
+      {
+        var font = Font.Default;
+
+        Device.OnPlatform(iOS: () =>
+        {
+          font = Font.SystemFontOfSize(NamedSize.Large, FontAttributes.Bold);
+        },
+          Android: () =>
+          {
+            font = Font.SystemFontOfSize(18, FontAttributes.Bold);
+          },
+          WinPhone: () =>
+          {
+            font = Font.SystemFontOfSize(24, FontAttributes.Bold);
+          });
+
+        return font;
+      }
+    }
+
+    public static Font Large
+    {
+      get
+      {
+        var font = Font.Default;
+
+        Device.OnPlatform(iOS: () =>
+        {
+          font = Font.SystemFontOfSize(NamedSize.Large);
+        },
+          Android: () =>
+          {
+            font = Font.SystemFontOfSize(18);
+          },
+          WinPhone: () =>
+          {
+            font = Font.SystemFontOfSize(24);
+          });
+
+        return font;
+      }
+    }
+
+    public static Font Medium
+    {
+      get
+      {
+        var font = Font.Default;
+
+        Device.OnPlatform(iOS: () =>
+        {
+          font = Font.SystemFontOfSize(NamedSize.Medium);
+        },
+          Android: () =>
+          {
+            font = Font.SystemFontOfSize(14);
+          },
+          WinPhone: () =>
+          {
+            font = Font.SystemFontOfSize(22);
+          });
+
+        return font;
+      }
+    }
+  }
+}
