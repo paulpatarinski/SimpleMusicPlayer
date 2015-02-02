60d5c7840f0f85fe60242ee22f9f27a53ddeae0e
Paul Patarinski <paulpatarinski@gmail.com>
2015-02-01 23:49:09 -0600

    Adding a better app icon

--- Android/Resources/Resource.designer.cs
+++ Android/Resources/Resource.designer.cs
@@ -173,9 +173,6 @@ namespace SimpleMusicPlayer.Android
 			// aapt resource value: 0x7f020002
 			public const int Icon = 2130837506;
 			
-			// aapt resource value: 0x7f020003
-			public const int refresh = 2130837507;
-			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
